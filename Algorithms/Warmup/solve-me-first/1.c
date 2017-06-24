0ec13292c70cbd7db335468a65b963595027f25c
myCodeHouse1 <mcodehouse@gmail.com>
2017-06-24 17:35:30 +0530

    Create 1.c

---
+++ Algorithms/Warmup/solve-me-first/1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+    // varible declaration
+    int a, b, sum;
+    // INPUT
+    scanf("%d %d", &a, &b);
+    
+    // OUTPUT
+    sum = a + b;
+    printf("%d" , sum);
+    
+return 0;
+}
